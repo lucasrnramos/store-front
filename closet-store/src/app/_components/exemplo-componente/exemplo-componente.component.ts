a2f0a0b420d2af5e1603be7b1851b55813442bf9
lucasrnramos <lucas.rodrigues@unimedmaceio.com.br>
2025-03-22 00:02:08 -0300

    first components

---
+++ closet-store/src/app/_components/exemplo-componente/exemplo-componente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplo-componente',
+  standalone: false,
+  templateUrl: './exemplo-componente.component.html',
+  styleUrl: './exemplo-componente.component.css'
+})
+export class ExemploComponenteComponent {
+
+}
