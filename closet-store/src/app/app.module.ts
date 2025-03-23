c3022ec20dafd99ee94ebbda14eca04127931cd5
lucasrnramos <lucas.rodrigues@unimedmaceio.com.br>
2025-03-23 19:23:05 -0300

    create base-ui component

--- closet-store/src/app/app.module.ts
+++ closet-store/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './_components/navbar/navbar.component';
 import { FooterComponent } from './_components/footer/footer.component';
+import { BaseUiComponent } from './_components/base-ui/base-ui.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    BaseUiComponent
   ],
   imports: [
     BrowserModule,
